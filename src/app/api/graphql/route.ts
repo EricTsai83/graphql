d13fd7556c6b6b57d6828c780a41e8cb368a2617
Eric Tsai <eric492718@gmail.com>
2023-08-01 02:10:20 +0800

    remove comment

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -30,29 +30,3 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   return handler(request);
 }
-// const handler = startServerAndCreateNextHandler<NextRequest>(server);
-// export async function GET(request: Request) {
-//   return new Response("This is my parent route");
-// }
-
-// export async function POST(request: NextRequest) {
-//   return handler(request);
-// }
-
-// const client = new ApolloClient({
-//   uri: "https://flyby-router-demo.herokuapp.com/",
-//   cache: new InMemoryCache(),
-// });
-
-// export async function get(request: NextRequest) {
-//   const formData = await request.formData();
-//   const file = formData.get("file");
-//   const response = await fetch(`${HOSTNAME}/akahana/vit-base-cats-vs-dogs`, {
-//     headers: HEADER,
-//     method: "POST",
-//     body: file,
-//   });
-
-//   const result = await response.json();
-//   return NextResponse.json(result);
-// }
